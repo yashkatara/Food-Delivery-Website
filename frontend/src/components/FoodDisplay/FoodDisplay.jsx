54da56725ce7edc7efc98cc933948ed37740558b
yashkatara <yash.katara@lifeasesolutions.com>
2026-02-03 21:47:21 +0530

    frontend

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react'
-import './FoodDisplay.css'
+import { useContext } from 'react';
 import { StoreContext } from '../../context/StoreContext';
 import FoodItem from '../FoodItem/FoodItem';
+import './FoodDisplay.css';
 
 const FoodDisplay = ({category}) => {
   const {food_list} = useContext(StoreContext);
@@ -10,7 +10,7 @@ const FoodDisplay = ({category}) => {
       <h2>Top Dishes Near you</h2>
       <div className="food-display-list">
         {food_list.map((item,index)=>{
-          return<FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image}/>
+          return <FoodItem key={index} id={item._id} name={item.name} price={item.price} description={item.description} image={item.image}/>
         })}
       </div>
       
